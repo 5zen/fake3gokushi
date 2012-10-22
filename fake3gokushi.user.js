0db141ad3a4f3e571663ce32d0d58318473fbf9d
5zen <haa85440@gmail.com>
2012-10-22 11:46:12 +0900

    update

--- fake3gokushi.user.js
+++ fake3gokushi.user.js
@@ -3,17 +3,7 @@
 // @version        2012.7.23
 // @namespace      mixi版 ブラウザ三国志の武将イラストを某ゲームのイラストに置き換えるスクリプトです。色々勝手に使ってます。ごめんなさい。
 // @icon           http://5zen.info/hokan/icon.png
-// @include        http://*.3gokushi.jp/*
-// @include        https://*.3gokushi.jp/*
-// @include        http://*.nexon.com/*
-// @include        http://*.3gokushi.jp/card/exhibit_list.php*
-// @include        http://*.3gokushi.jp/card/bid_list.php*
-// @include        http://*.3gokushi.jp/card/busyobook_picture.php*
-// @include        http://*.3gokushi.jp/busyodas/busyodas.php*
-// @include        http://*.3gokushi.jp/busyodas/b3kuji.php*
-// @include        http://*.3gokushi.jp/alliance/alliance_log.php*
-// @require	   http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
-// @description    mixi版 ブラウザ三国志の武将イラストを某ゲームのイラストに置き換えるスクリプトです。色々勝手に使ってます。ごめんなさい。
+// ==/UserScript==
 //
 //----更新履歴----
 // 2012.05.09	美羽先生の口調修正・アイテムショップの画像変更
